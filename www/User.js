13f27f54e1caf4d57e1f4db7d5eb3742b0ed6fce
Samir-002 <142381504+Samir-002@users.noreply.github.com>
2023-08-16 12:27:36 +0530

    Roys web

---
+++ www/User.js
@@ -0,0 +1,43 @@
+var a = "Samir"
+var b = "Golu"
+var c = "Harsh"
+var d = "Prity"
+var e = "Sakshi"
+var f = "Ruchi"
+var g = "Mannu"
+               
+pass = prompt("Enter your name that I verify its you");
+
+                if(pass==b)
+                {
+                    window.location="Golu.html";
+                }
+                else
+                {
+                    window.location="index.html"
+                }
+                if(pass==a)
+                {
+                    window.location="Samir.html";
+                }
+                if(pass==c)
+                {
+                    window.location="Harsh.html"
+                }
+                if(pass==d)
+                {
+                    window.location="Prity.html";
+                }
+                if(pass==e)
+                {
+                    window.location="Sakshi.html"
+                }
+                if(pass==f)
+                {
+                    window.location="Ruchi.html"
+                }
+                if (pass==g) {
+                    window.location="Mannu.html"
+                }
+
+                
